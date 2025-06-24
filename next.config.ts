a3d4d030de737a5ef2f451b35e2cbb3897e93391
Sitthiphong Krobkrong (Fram) <Sitthiphong.k@betimes.biz>
2025-06-24 17:23:53 +0700

    update : proxy_pass api

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://3.106.213.14:5000/api/:path*`, // Proxy to Backend''
+      }
+    ];
   }
 };
 
